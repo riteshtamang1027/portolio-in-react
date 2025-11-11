8ff556fe239784e35c6c1dcc692d966bfc4cdecd
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-11-11 22:07:21 +0545

    add css amd make it more effective

--- portfolio_frontend/src/components/heroSection.jsx
+++ portfolio_frontend/src/components/heroSection.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import Tooltip from "@mui/material/Tooltip";
 import { Facebook, Github, Instagram, Linkedin } from "lucide-react";
+import { Typewriter } from "react-simple-typewriter";
 export default function heroSection() {
   return (
     <div className="xl:px-64 lg:px-32 md:px-16 px-8 text-white h-full">
@@ -20,8 +21,20 @@ export default function heroSection() {
                 <p className="text-xl font-bold opacity-90">Hi, I'm</p>
                 <p className="text-3xl font-bold text-cyan-500">{`Ritesh Tamang`}</p>
               </div>
-              <p className="text-2xl font-semibold">
-                I'm <span>Frontend Developer.</span>{" "}
+              <p className="text-2xl font-bold">
+                I'm{" "}
+                <span className="text-cyan-500">
+                  {" "}
+                  <Typewriter
+                    words={["Frontend Developer", "Learning Next.Js"]}
+                    loop={Infinity}
+                    cursor
+                    cursorStyle="|"
+                    typeSpeed={100}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                  />
+                </span>{" "}
               </p>
               <p className="font-semibold opacity-80 trackind-wide">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
@@ -32,18 +45,37 @@ export default function heroSection() {
 
             {/* button section */}
             <div className="flex items-center gap-12">
-              <button className="border px-4 py-2 rounded-full text-sm font-semibold bg-cyan-500 text-black/60 cursor-pointer border-white">
-                Download CV
+              {/* CV download button */}
+              <button className="relative overflow-hidden px-4 py-2 rounded-full text-sm font-semibold text-black/60 bg-cyan-500/80 cursor-pointer border-transparent border transition-all duration-1000 hover:scale-105 hover:text-cyan-500 hover:border-cyan-600 hover:bg-transparent">
+                <span className="relative z-10">Download CV</span>
+                <span className="absolute inset-0 bg-cyan-500/80 transform -translate-x-full hover:translate-x-0 transition-transform duration-1000 delay-150 rounded-full"></span>
               </button>
 
               {/* social media sections */}
               <div className="flex items-center gap-4">
                 {mediaIcon.map((Icon, index) => (
-                  <div
-                    key={index}
-                    className="border p-2 rounded-full border-cyan-500 cursor-pointer"
-                  >
-                    <Icon width={20} height={20} className="text-cyan-500" />
+                  <div key={index}>
+                    <Tooltip
+                      title={Icon.i_name}
+                      placement="top"
+                      componentsProps={{
+                        tooltip: {
+                          sx: {
+                            bgcolor: "#06b6d4", // tooltip background
+                            color: "black", // tooltip text
+                            fontSize: "0.7rem",
+                          },
+                        },
+                      }}
+                    >
+                      <div className="border p-2 rounded-full border-cyan-500 cursor-pointer transition-transform hover:scale-110  hover:ring-cyan-300 hover:ring-2 hover:ring-offset-1 hover:ring-offset-transparent duration-300">
+                        <Icon.icon
+                          width={20}
+                          height={20}
+                          className="text-cyan-500 "
+                        />
+                      </div>
+                    </Tooltip>
                   </div>
                 ))}
               </div>
@@ -53,7 +85,7 @@ export default function heroSection() {
           {/* Image container section */}
           <div className="w-1/3">
             <img
-              className="rounded-full "
+              className="rounded-full"
               src="https://images.unsplash.com/photo-1654110455429-cf322b40a906?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=600"
               alt="Image"
             />
@@ -64,4 +96,21 @@ export default function heroSection() {
   );
 }
 
-const mediaIcon = [Facebook, Linkedin, Instagram, Github];
+const mediaIcon = [
+  {
+    icon: Facebook,
+    i_name: "Facebook",
+  },
+  {
+    icon: Linkedin,
+    i_name: "Linkedin",
+  },
+  {
+    icon: Instagram,
+    i_name: "Instagram",
+  },
+  {
+    icon: Github,
+    i_name: "Github",
+  },
+];
--- portfolio_frontend/src/components/navbar.jsx
+++ portfolio_frontend/src/components/navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='xl:px-64 lg:px-32 md:px-16 px-8'>
-     {/* <p className='text-white text-4xl font-bold'>Port <span className='text-cyan-500'>Folio</span></p> */}
+    <div className='w-88 p-4 bg-gray-600'>
+
     </div>
   )
 }
