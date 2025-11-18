84b81a58bca9ddbd3c139a6872e70e385ceaa08e
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-11-18 22:05:27 +0545

    successfully add toggle theme in footer bottom  section

--- portfolio_frontend/src/components/footer_bottom_section.jsx
+++ portfolio_frontend/src/components/footer_bottom_section.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Footer_bottom_section() {
   return (
 
-       <div className="mt-12 border-t border-cyan-700 pt-6 text-center text-gray-400 text-sm flex flex-col md:flex-row justify-between items-center gap-2">
+       <div className="mt-12 border-t border-[var(--border-radious)] pt-6 text-center text-[var(--text-primary)]/50 text-sm flex flex-col md:flex-row justify-between items-center gap-2">
         <span>
           &copy; {new Date().getFullYear()} Ritesh Tamang. All rights reserved.
         </span>
-        <span className="text-gray-500 text-xs">
+        <span className="text-[var(--text-primary)]/50 text-xs">
           Made with ❤️ and React + Tailwind
         </span>
       </div>
