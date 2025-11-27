b2d3a940fccd27aec1be964897688e70a92e7da0
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-11-27 23:18:21 +0545

    fixed mongo db connection

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+
 const connectDB = async () => {
   try {
-    mongoose.connect(
-      process.env.MONGODB_URL
-    );
-
-    console.log("MongoDB connected successfully.", {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.log("Connected failed.", error);
+    console.error("MongoDB connection failed:", error);
+    process.exit(1); // stop server if DB fails
   }
 };
 
